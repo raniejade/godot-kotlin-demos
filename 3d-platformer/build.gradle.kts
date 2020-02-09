e996d8e103630f26cb3619ba72988eab12ecb023
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2020-02-09 13:00:11 +1100

    Apply godot plugin

---
+++ 3d-platformer/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+  kotlin("multiplatform")
+  id("com.github.raniejade.godot-kotlin")
+}
+
+godot {
+  libraries {
+    val platformer by creating {
+
+    }
+  }
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,20 @@
-plugins {
-  kotlin("multiplatform") version "1.3.61" apply false
+buildscript {
+  repositories {
+    // plugin is not available in gradlePluginPortal yet
+    maven("https://dl.bintray.com/raniejade/godot-kotlin")
+    jcenter()
+  }
+
+  dependencies {
+    classpath(kotlin("gradle-plugin", version = "1.3.61"))
+    classpath("com.github.raniejade:godot-kotlin-gradle-plugin:32.1.0-rc.2")
+  }
+}
+
+subprojects {
+  repositories {
+    // godot-kotlin artifacts are not in jcenter yet
+    maven("https://dl.bintray.com/raniejade/godot-kotlin")
+    jcenter()
+  }
 }
